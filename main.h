7266042b147aa16609c2d41a922f02e038b47490
Axel <Axel.palombo.ap@gmail.com>
2024-07-15 16:32:52 +0000

    project created

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+int _printf(const char *format, ...);
+/* Acá van los prototypes de las conversion functions */
+
+
+#endif /* MAIN_H */
---
+++ printf.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _printf - Prints formated values
+ * @format: String composed of zero or more directives (example: %i)
+ * Return: Always 0 Success
+ */
+int _printf(const char *format, ...)
+{
+	// 1- Recorrer format, contar directives con un Switch
+	// 2- Si fcounter > 0 => Paso 4
+	// 3- Sino printear mensaje completo
+	// 4- Formar Array con directives (Malloc = fcounter)
+	// 5- Manejar Return de Malloc
+	// 6- Recorrer directives con parÃmetros de la funcon
+	// 7- Calcular Bytes de memoria de cada parametro (convertido a string)
+	// 8- Malloc para Array de punteros a Strings
+	// 9- Paso 6 de nuevo
+	// 10- Utilizar un Switch para cada Case => Conversion Funct 
+	// 11- Conversion Funct retorna String lo que se le pase
+	// 12- Agregar retorno de funcion a un Array de punteros
+	// 13- Calcular bmemory para String final (Format + Parametros(Strings))
+	// 14- Write (String final, bmemory)
+	// 15- Return 0 Success
+	//
+	// Conversion_Funct.c
+	// 1- Char to String
+	// 2- Int to String (All test)
+}
